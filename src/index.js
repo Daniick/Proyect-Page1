bb741f426c20209e8fb6fbdb9619517c7405bc7f
david <david.ortega.pajaro1@gmail.com>
2023-05-26 06:47:16 -0500

    nmp added

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './script';
+import './index.css';
+import '../node_modules/normalize.css/normalize.cs'
--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import './style.css';
+
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
